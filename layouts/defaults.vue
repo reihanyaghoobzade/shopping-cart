4e62b5e2958c9a302826459fb2a9bcb65988807b
Reihane Yaghoobzade <reihanygh2002@gmail.com>
2022-08-27 10:58:56 +0430

    initial commit

---
+++ layouts/defaults.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="flex flex-col justify-between items-center min-h-screen w-full">
+    <nuxt />
+  </div>
+</template>
+<script>
+export default {
+  name: 'DefaultLayout',
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,60 @@
+export default {
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: 'سبد خرید',
+        htmlAttrs: {
+            lang: 'fa',
+            dir: 'rtl'
+        },
+        meta: [
+            { charset: 'utf-8' },
+            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+            { hid: 'description', name: 'description', content: '' },
+            { name: 'format-detection', content: 'telephone=no' },
+        ],
+        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+            {
+                rel: 'stylesheet',
+                href: 'https://cdn.jsdelivr.net/gh/rastikerdar/vazirmatn@v33.003/Vazirmatn-font-face.css',
+                type: 'text/css',
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css',
+            },
+        ],
+        script: [{ src: 'https://cdn.tailwindcss.com' }],
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: ['~assets/main.css'],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+        // https://go.nuxtjs.dev/tailwindcss
+        '@nuxtjs/tailwindcss',
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/axios
+        '@nuxtjs/axios',
+    ],
+
+    // Axios module configuration: https://go.nuxtjs.dev/config-axios
+    axios: {
+        // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+        baseURL: '/',
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+}
